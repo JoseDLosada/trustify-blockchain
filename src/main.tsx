44dea78993d089ea425ffd5460b4e06eecfe6e26
JLosada-Dev <jd.losada.dev@gmail.com>
2024-11-16 09:18:20 -0500

    refactor main.tsx to clean up imports and formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-// import App from './App.tsx'
-import TrustifyLayout from './TrustifyLayout.tsx'
-import TrustifyLogin from './TrustifyLogin.tsx'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
-// import TrustifyLogin from './TrustifyLogin.tsx'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-    <App></App>
+      <App></App>
     </BrowserRouter>
-  
-  </StrictMode>,
-)
+  </StrictMode>
+);
